b987931a0bafe07090d6b1f6ef4b22979a83bf44
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-11-26 14:01:50 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 3 Найти s = min(a, b) + min(c, d), 
+            //используя вспомогательные функции(ввода аргументов, нахождения минимальной величины, вывода результата).
+            int a = Input("a = ");
+            int b = Input("b = ");
+            int c = Input("c = ");
+            int d = Input("d = ");
+            int s = Min(a, b) + Min(c, d);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+        static int Input(string msg)
+        {
+            Console.Write(msg);
+            return int.Parse(Console.ReadLine());
+        }
+        static int Min(int x, int y)
+        {
+            return x< y ? x : y;
+        }
+    }
+}
