45a14732130f82c01d440a5f637b956adb35afc5
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-11-26 13:39:59 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1  Найти минимальную величину из двух целых переменных a, b,
+            //используя вспомогательные методы (ввода аргументов, нахождения минимальной величины, вывода результата).
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Минимальная величина = {Min(x, y)}");
+            Console.ReadKey();
+        }
+        static int Min(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+    }
+}
