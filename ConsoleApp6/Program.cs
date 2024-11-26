24014b40ac2b39b6eacf582d1bc96431b877bb7b
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-11-26 14:16:58 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определить статический метод int Meters(int k, int m), который принимает два целых аргумента
+            //(километры k, метры m) и возвращает количество метров. 
+            //Используя этот метод, вычислить количество метров для расстояния:
+            //а) 3 км 40 м;
+            //б) 12 км 540 м.
+            int k = Input("kilometers = ");
+            int m = Input("meters = ");
+            int mt = Meters(k, m);
+            Console.WriteLine($"Общее кол-во метров = {mt}");
+            Console.ReadKey();
+        }
+        static int Meters(int k, int m)
+        {
+            return (k * 1000) +m;
+        }
+        static int Input(string x)
+        {
+            Console.Write(x);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
