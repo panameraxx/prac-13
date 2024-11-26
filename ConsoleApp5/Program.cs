56fdcf8cef7cbd6e9f371b42889f939329cf8d7e
panameraxx <90213411+panameraxx@users.noreply.github.com>
2024-11-26 14:12:09 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определить статический метод int Seconds(int h, int m, int s), который принимает три целых аргумента
+            //(часы h, минуты m и секунды s) и возвращает количество секунд, прошедших с начала дня.
+            //Используя этот метод, вычислить количество секунд, прошедших с начала дня при:
+            //а) 0 ч 40 мин 20 с
+            //б) 3 ч 10 мин 50 с
+            int h = Input("hours = ");
+            int m = Input("minutes = ");
+            int s = Input("seconds = ");
+            int sec = Seconds(h, m, s);
+            Console.WriteLine($"Время за день в секундах = {sec}");
+            Console.ReadKey();
+        }
+        static int Seconds(int h, int m, int s)
+        {
+            return (h * 3600) + (m * 60) + s;
+        }
+        static int Input(string x)
+        {
+            Console.Write(x);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
